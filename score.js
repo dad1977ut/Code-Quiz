e0cde4294295c9b840919e2b5d1522821f2b45b0
Dameon Davis <dad1977ut@yahoo.com>
2021-01-07 20:15:45 -0700

    display score

--- score.js
+++ score.js
@@ -1,2 +1,9 @@
 var highScore = JSON.parse(localStorage.getItem("scoreArr"));
+var displayScore = document.querySelector("#high-score");
 console.log(highScore);
+for (let i = 0; i < highScore.length; i++) {
+  var score = highScore[i];
+  var li = document.createElement("li");
+  li.textContent = score;
+  displayScore.appendChild(li);
+}
